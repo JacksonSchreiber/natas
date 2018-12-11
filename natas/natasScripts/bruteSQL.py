a7712b041a83c6c447caba28550ab379c1713148
root <root@localhost.localdomain>
2018-12-10 20:39:51 -0800

    the script

---
+++ natas/natasScripts/bruteSQL.py
@@ -0,0 +1,9 @@
+#!/bin/python3.7
+import requests
+
+url = 'http://natas15.natas.labs.overthewire.org/index.php'
+
+#data is what is inputted into the Username: box. test is inputted in this case
+r = requests.post(url, auth=('natas15', 'AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J'), data={'username': 'test'})
+
+print(r.text)
