fb5ad9414b00a1cc38b3d2c0169dff8321be0944
JacksonSchreiber <jacksonschreiber@hotmail.com>
2018-12-10 21:29:56 -0800

    new username

--- natas/natasScripts/bruteSQL.py
+++ natas/natasScripts/bruteSQL.py
@@ -4,7 +4,6 @@ import requests
 url = 'http://natas15.natas.labs.overthewire.org/index.php'
 
 #data is what is inputted into the Username: box. test is inputted in this case
-r = requests.post(url, auth=('natas15', 'AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J'), data={'username': 'test'})
+r = requests.post(url, auth=('natas15', 'AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J'), data={'username': 'natas16'})
 
 print(r.text)
-#test
